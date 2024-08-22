513eacb30c190ea8490fd88a7a2c73f9d04d1253
Subba Rao <Subba.Rao@philips.com>
2024-08-22 14:02:24 +0530

    fix test failure

--- tshirts.cpp
+++ tshirts.cpp
@@ -1,13 +1,14 @@
 #include <string>
 
 std::string classify_tshirt(int size) {
-    std::string sizeName = "";
+    std::string sizeName;
     if (size <= 38) {
         sizeName = "Small";
-    } else if (size > 38 && size < 42) {
+    }
+    else if (size < 42) {
         sizeName = "Medium";
     }
-    else if (size >= 42) {
+    else {
         sizeName = "Large";
     }
     return sizeName;
