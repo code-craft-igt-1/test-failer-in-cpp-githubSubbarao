e4437ad2ce96b076d6c4d03f36ccfe5bbef54195
Subba Rao <Subba.Rao@philips.com>
2024-08-22 14:09:15 +0530

    fix warnings

--- tshirts.cpp
+++ tshirts.cpp
@@ -1,17 +1,15 @@
 #include <string>
 
-std::string classify_tshirt(int size) 
+std::string classify_tshirt(int size)
 {
     std::string sizeName;
-    if (size <= 38) 
+    if (size <= 38)
     {
         sizeName = "Small";
-    }
-    else if (size < 42) 
+    } else if (size < 42)
     {
         sizeName = "Medium";
-    }
-    else 
+    } else
     {
         sizeName = "Large";
     }
