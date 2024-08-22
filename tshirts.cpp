b594acfa9d798f1f5fb3a49b544af3840fd400b7
Subba Rao <Subba.Rao@philips.com>
2024-08-22 13:20:11 +0530

    fixed test cases

--- tshirts.cpp
+++ tshirts.cpp
@@ -1,13 +1,11 @@
 #include <string>
 
 std::string classify_tshirt(int size) {
-    if (size < 38) {
+    if (size <= 38) {
         return "Small";
-    } else if (size < 42) {
+    } else if (size > 38 && size < 42) {
         return "Medium";
-    } else if (size < 46) {
+    } else if (size > = 42) {
         return "Large";
-    } else {
-        return "Extra Large";
-    }
+    } 
 }
--- weatherreport.cpp
+++ weatherreport.cpp
@@ -37,20 +37,12 @@ std::string Report(const IWeatherSensor& sensor) {
 
     std::string report = "Sunny day";
 
-    if (temperature > 25) {
-        if (precipitation >= 20 && precipitation < 60) {
+    if (sensor.TemperatureInC() > 25) {
+        if (precipitation >= 20 && precipitation < 60)
             report = "Partly cloudy";
-        } else if (precipitation >= 60) {
-            report = "Rainy day";
-        }
-    } else if (temperature <= 25) {
-        if (humidity > 70) {
-            report = "Humid day";
-        } else if (windSpeed > 50) {
-            report = "Windy day";
-        }
+        else if (sensor.WindSpeedKMPH() > 50)
+            report = "Alert, Stormy with heavy rain";
     }
-
     return report;
 }
 }  // namespace WeatherSpace
